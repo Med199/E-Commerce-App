ea7773b626d93b91daa01f1d4389082e9155c753
med199 <mohamed.bekour2018@gmail.com>
2026-04-17 15:26:15 +0200

    Implement JWT authentication with sign-in and sign-up endpoints

--- src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
@@ -5,7 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
     Category findByCategoryName(@NotBlank() @Size(min=5,message = "Category Name must contain at least 2 Carecters") String categoryName);
 }
